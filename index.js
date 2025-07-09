4bd5d2e87987794e8908a56e425ea32437fea604
MSadawi <sadawe147@gmail.com>
2025-07-09 17:46:40 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+});
